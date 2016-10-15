5bbbdce93547c84299924a04ff247e66fba7b373
lodewijckvogelzang <lodewijckvogelzang@msn.com>
2016-10-15 14:05:43 +0200

    Implemented MasterDetail page as entry page.

---
+++ LovoForms/Master/Master.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Xamarin.Forms;
+
+namespace LovoForms
+{
+	public partial class Master : ContentPage
+	{
+		public ListView ListView { get { return listView; } }
+
+		public Master()
+		{
+			Title = "Forms";
+			InitializeComponent();
+		}
+	}
+}
---
+++ LovoForms/MasterDetail.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace LovoForms
+{
+	public class MasterDetail : MasterDetailPage
+	{
+		public MasterDetail()
+		{
+			var masterPage = new Master();
+			var detailPage = new Detail();
+
+			Master = masterPage;
+			Detail = new NavigationPage(detailPage);
+		}
+	}
+}
