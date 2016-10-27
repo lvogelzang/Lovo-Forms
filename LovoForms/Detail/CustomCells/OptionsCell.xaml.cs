c147e5ba592e7358d4d653138fbfbe43e8eda6bd
lodewijckvogelzang <lodewijckvogelzang@msn.com>
2016-10-27 16:57:42 +0200

    Initialized custom cell containing options

---
+++ LovoForms/Detail/CustomCells/OptionsCell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace LovoForms
+{
+	public partial class OptionsCell : ContentView
+	{
+		public OptionsCell()
+		{
+			InitializeComponent();
+		}
+	}
+}
